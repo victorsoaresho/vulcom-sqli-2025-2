d05cda23d0c64ff66b3c8a165d10c9fe7ceb27dc
Victor Soares <victorsoaresho@gmail.com>
2025-09-10 21:46:12 -0300

    10/09 Correção da vulnerabilidade SQL injection

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = new sqlite3.Database(':memory:');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-// Criar tabela e inserir dados vulneráveis
+// Criar tabela e inserir dados
 db.serialize(() => {
     db.run("CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
     db.run("INSERT INTO users (username, password) VALUES ('admin', 'admin123')");
@@ -20,26 +20,33 @@ db.serialize(() => {
     db.run("INSERT INTO flags (flag) VALUES ('VULCOM{SQLi_Exploit_Success}')");
 });
 
-// Rota de login com SQL Injection
+// Rota de login
 app.get('/', (req, res) => {
     res.render('login');
 });
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    
-    // CONSULTA SQL VULNERÁVEL 🚨
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
 
-    // Consegui fazer passando o admin'-- no user a qualquer coisa na password //
-    db.all(query, [], (err, rows) => {
+    // CONSULTA SEGURA ✅ (sem SQL Injection)
+    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    const query2 = 'SELECT flag FROM flags';
+
+    db.all(query, [username, password], (err, rows) => {
         if (err) {
             return res.send('Erro no servidor');
         }
         if (rows.length > 0) {
             console.log('CONSULTA: ', query);
             console.log('RESULTADO:', rows);
-            return res.send(`Bem-vindo, ${username}! <br> Flag: VULCOM{SQLi_Exploit_Success}`);
+
+            db.get(query2, [], (err, flagRow) => {
+                if (err) return res.send(`Erro ${err}`);
+
+                let ret = `Bem-vindo, ${username}! <br>`;
+                ret += `Flag: ${flagRow.flag}`;
+                return res.send(ret);
+            });
         } else {
             return res.send('Login falhou!');
         }
